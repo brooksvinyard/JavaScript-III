4de1c778e2a90b1c1c9d6babcf6c2035aeab3274
brooksvinyard <brooksvinyard@BrooksVsMacBook-10.hsd1.ca.comcast.net>
2019-01-23 14:17:40 -0800

    finished up the questions about using this

--- assignments/this.js
+++ assignments/this.js
@@ -19,37 +19,42 @@
 // Principle 2
 
 // code example for Implicit Binding
+
 /*
-const hobbit = {
-    name: 'Samwise',
-    food: 'taters',
+const person = {
+    name: 'Fred',
+    food: 'apples',
     cook: function() {
      console.log(`${this.name} loves to eat ${this.food}`) 
     }
   }
   
-  hobbit.cook();
+person.cook();
 */
+
 
 
 // Principle 3
 
 // code example for New Binding
 
+/*
+function Student(attributes) {
+    this.name = attributes.name
+    this.fav_class = attributes.fav_class;
+  }
+  
+Student.prototype.greet = function() {
+    return `${this.name} likes the class: ${this.fav_class}`;
+  }
 
+const bob = new Student({
+    name: 'Bob',
+    fav_class: 'English',
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(bob.greet());
+*/
 
 
 
@@ -60,21 +65,14 @@ const hobbit = {
 /*
 const person = {
     name: 'Sarah'
-  }
+}
+
+const skills = ['HTML', 'CSS', 'JS'];
   
-  const hobbit = {
-    name: 'Frodo'
-  }
-  
-  const skills = ['HTML', 'CSS', 'JS'];
-  
-  function introduce(skills1, skills2, skills3) {
-    
+function introduce(skills1, skills2, skills3) {    
     console.log(`Hello!  My name is ${this.name}, and I have skills in: ${skills1}, ${skills2}, ${skills3}`)
-  }
+}
   
-  // call has one argument list
-  introduce.call(person, skills);
-  // apply accepts an array
-  introduce.apply(person, skills);
-  */
+introduce.apply(person, skills);
+
+*/
